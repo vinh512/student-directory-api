d661aa678a6a2fa32b1ca7313eb912f5bacdc1e9
vinh huynh <vvvhuynh@yahoo.com>
2016-12-09 11:06:12 -0600

    added .gitignore file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var Student = require('../models/blogpost');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
